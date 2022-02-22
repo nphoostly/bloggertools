20fd46926302dc0465fc1b7e333f2551579ac889
Sushant Karn <99542426+sushantkarn773@users.noreply.github.com>
2022-02-22 14:12:32 +0545

    Create main.js

---
+++ main.js
@@ -0,0 +1,2 @@
+ function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+  loadCSS("https://kit-pro.fontawesome.com/releases/v6.0.0/css/pro.min.css");
